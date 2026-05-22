94760f2b38d9654fc866e3c3d56b7b469909187d
Cristian Ciro <cristian_ciro@outlook.es>
2026-05-21 19:22:52 -0500

    spawn_power_monitor simulator/production mode implemented

--- crates/hpd-netlink/src/lib.rs
+++ crates/hpd-netlink/src/lib.rs
@@ -1,81 +1,108 @@
-use futures_util::StreamExt;
 use hpd_core::transition::Transition;
 use tokio::sync::mpsc;
-use tokio_udev::{AsyncMonitorSocket, MonitorBuilder};
 use tracing::{error, info, debug};
 
-const SUBSYS_POWER: &str = "power_supply";
-const PROP_ONLINE: &str = "POWER_SUPPLY_ONLINE";
-const VAL_ONLINE_TRUE: &str = "1";
-const IDENTIFIER_AC: &str = "AC";
-const IDENTIFIER_ADP: &str = "ADP";
+// ========================================================
+// PRODUCTION MODE (Compily on Linux)
+// ========================================================
+#[cfg(target_os = "linux")]
+mod linux {
+    use super::*;
+    use futures_util::StreamExt;
+    use tokio_udev::{AsyncMonitorSocket, MonitorBuilder};
 
-pub async fn spawn_power_monitor(tx: mpsc::Sender<Transition>) {
-    info!("Starting Netlink monitor (udev) for energy events...");
+    const SUBSYS_POWER: &str = "power_supply";
+    const PROP_ONLINE: &str = "POWER_SUPPLY_ONLINE";
+    const VAL_ONLINE_TRUE: &str = "1";
+    const IDENTIFIER_AC: &str = "AC";
+    const IDENTIFIER_ADP: &str = "ADP";
 
-    // 1. Only detect changes in "power_supply"
-    let builder = match MonitorBuilder::new() {
-        Ok(b) => b,
-        Err(e) => {
-            error!("Failure creating udev monitor: {}", e);
-            return;
-        }
-    };
+    pub async fn spawn_power_monitor(tx: mpsc::Sender<Transition>) {
+        info!("Starting Netlink monitor (udev) for energy events...");
 
-    let builder = match builder.match_subsystem(SUBSYS_POWER) {
-        Ok(b) => b,
-        Err(e) => {
-            error!("Failure getting power_supply subsystem: {}", e);
-            return;
-        }
-    };
+        // 1. Only detect changes in "power_supply"
+        let builder = match MonitorBuilder::new() {
+            Ok(b) => b,
+            Err(e) => {
+                error!("Failure creating udev monitor: {}", e);
+                return;
+            }
+        };
 
-    // 2. Open Netlink socket
-    let monitor = match builder.listen() {
-        Ok(m) => m,
-        Err(e) => {
-            error!("Failure with socket udev detection: {}", e);
-            return;
-        }
-    };
+        let builder = match builder.match_subsystem(SUBSYS_POWER) {
+            Ok(b) => b,
+            Err(e) => {
+                error!("Failure getting power_supply subsystem: {}", e);
+                return;
+            }
+        };
 
-    let mut async_monitor = match AsyncMonitorSocket::new(monitor) {
-        Ok(m) => m,
-        Err(e) => {
-            error!("Failure convertion with async monitor: {}", e);
-            return;
-        }
-    };
+        // 2. Open Netlink socket
+        let monitor = match builder.listen() {
+            Ok(m) => m,
+            Err(e) => {
+                error!("Failure with socket udev detection: {}", e);
+                return;
+            }
+        };
 
-    debug!("Netlink monitor ready. Awaiting for AC/DC events...");
+        let mut async_monitor = match AsyncMonitorSocket::new(monitor) {
+            Ok(m) => m,
+            Err(e) => {
+                error!("Failure convertion with async monitor: {}", e);
+                return;
+            }
+        };
 
-    // 3. Infinite sleeping loop (0% CPU). Only awake when event happen
-    while let Some(event_result) = async_monitor.next().await {
-        match event_result {
-            Ok(event) => {
-                // Kernel notify an event. Check if AC/ADP
-                if let Some(sysname) = event.sysname() {
-                    let name = sysname.to_string_lossy().to_uppercase();
-                    
-                    if name.contains(IDENTIFIER_AC) || name.contains(IDENTIFIER_ADP) {
+        debug!("Netlink monitor ready. Awaiting for AC/DC events...");
+
+        // 3. Infinite sleeping loop (0% CPU). Only awake when event happen
+        while let Some(event_result) = async_monitor.next().await {
+            match event_result {
+                Ok(event) => {
+                    // Kernel notify an event. Check if AC/ADP
+                    if let Some(sysname) = event.sysname() {
+                        let name = sysname.to_string_lossy().to_uppercase();
                         
-                        // ONLINE (1 = Connected, 0 = Disconnected)
-                        if let Some(online_val) = event.property_value(PROP_ONLINE) {
-                            let is_ac_plugged: bool = online_val == VAL_ONLINE_TRUE;
+                        if name.contains(IDENTIFIER_AC) || name.contains(IDENTIFIER_ADP) {
                             
-                            info!("⚡ Hardware event detected: Charger connected = {}", is_ac_plugged);
+                            // ONLINE (1 = Connected, 0 = Disconnected)
+                            if let Some(online_val) = event.property_value(PROP_ONLINE) {
+                                let is_ac_plugged: bool = online_val == VAL_ONLINE_TRUE;
+                                
+                                info!("⚡ Hardware event detected: Charger connected = {}", is_ac_plugged);
 
-                            if tx.send(Transition::AcPowerChanged(is_ac_plugged)).await.is_err() {
-                                error!("Netlink monitor: Main executor not available. Stopping monitor.");
-                                break;
+                                if tx.send(Transition::AcPowerChanged(is_ac_plugged)).await.is_err() {
+                                    error!("Netlink monitor: Main executor not available. Stopping monitor.");
+                                    break;
+                                }
                             }
                         }
                     }
                 }
-            }
-            Err(e) => {
-                error!("Failure detecting Netlink event: {}", e);
+                Err(e) => {
+                    error!("Failure detecting Netlink event: {}", e);
+                }
             }
         }
     }
 }
+
+// ========================================================
+// SIMULATOR MODE (Compile on macOS)
+// ========================================================
+#[cfg(not(target_os = "linux"))]
+mod dummy {
+    use super::*;
+    pub async fn spawn_power_monitor(_tx: mpsc::Sender<Transition>) {
+        info!("AC Monitor disabled on macOS (Simulator mode).");
+        // Sleeping thread without CPU consume
+        std::future::pending::<()>().await;
+    }
+}
+
+#[cfg(target_os = "linux")]
+pub use linux::spawn_power_monitor;
+
+#[cfg(not(target_os = "linux"))]
+pub use dummy::spawn_power_monitor;
